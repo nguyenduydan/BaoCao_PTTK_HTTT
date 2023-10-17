9c0e5b51eb883b49a69ebf6f8aafb65f3b386f45
FOG <dn135897@gmail.com>
2023-10-17 22:44:23 +0700

    update

---
+++ QLKH.sql
@@ -0,0 +1,545 @@
+/*==============================================================*/
+/* DBMS name:      Microsoft SQL Server 2008                    */
+/* Created on:     17/10/2023 10:19:28 CH                       */
+/*==============================================================*/
+
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('INVOICE_PAYMENT') and o.name = 'FK_INVOICE__BILLS_SUPPLIER')
+alter table INVOICE_PAYMENT
+   drop constraint FK_INVOICE__BILLS_SUPPLIER
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('INVOICE_PAYMENT') and o.name = 'FK_INVOICE__DETAILS_ORDERS_D')
+alter table INVOICE_PAYMENT
+   drop constraint FK_INVOICE__DETAILS_ORDERS_D
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('ITEMS') and o.name = 'FK_ITEMS_ITEMS_SUPPLIER')
+alter table ITEMS
+   drop constraint FK_ITEMS_ITEMS_SUPPLIER
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('ITEMS') and o.name = 'FK_ITEMS_ITEMS2_PRODUCTS')
+alter table ITEMS
+   drop constraint FK_ITEMS_ITEMS2_PRODUCTS
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('PRODUCTS') and o.name = 'FK_PRODUCTS_REPORT_REPORT_F')
+alter table PRODUCTS
+   drop constraint FK_PRODUCTS_REPORT_REPORT_F
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('PRODUCTS2') and o.name = 'FK_PRODUCTS_PRODUCTS_PRODUCTS')
+alter table PRODUCTS2
+   drop constraint FK_PRODUCTS_PRODUCTS_PRODUCTS
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('PRODUCTS2') and o.name = 'FK_PRODUCTS_PRODUCTS2_SHELF')
+alter table PRODUCTS2
+   drop constraint FK_PRODUCTS_PRODUCTS2_SHELF
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('SEND') and o.name = 'FK_SEND_SEND_ORDERS_D')
+alter table SEND
+   drop constraint FK_SEND_SEND_ORDERS_D
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('SEND') and o.name = 'FK_SEND_SEND2_SUPPLIER')
+alter table SEND
+   drop constraint FK_SEND_SEND2_SUPPLIER
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('STORAGE') and o.name = 'FK_STORAGE_STORAGE_PRODUCTS')
+alter table STORAGE
+   drop constraint FK_STORAGE_STORAGE_PRODUCTS
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('STORAGE') and o.name = 'FK_STORAGE_STORAGE2_INVENTOR')
+alter table STORAGE
+   drop constraint FK_STORAGE_STORAGE2_INVENTOR
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('INVENTORY_LEVEL')
+            and   type = 'U')
+   drop table INVENTORY_LEVEL
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('INVOICE_PAYMENT')
+            and   name  = 'BILLS_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index INVOICE_PAYMENT.BILLS_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('INVOICE_PAYMENT')
+            and   name  = 'DETAILS_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index INVOICE_PAYMENT.DETAILS_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('INVOICE_PAYMENT')
+            and   type = 'U')
+   drop table INVOICE_PAYMENT
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('ITEMS')
+            and   name  = 'ITEMS2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index ITEMS.ITEMS2_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('ITEMS')
+            and   name  = 'ITEMS_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index ITEMS.ITEMS_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ITEMS')
+            and   type = 'U')
+   drop table ITEMS
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ORDERS_DETAIL')
+            and   type = 'U')
+   drop table ORDERS_DETAIL
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PRODUCTS')
+            and   name  = 'REPORT_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index PRODUCTS.REPORT_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('PRODUCTS')
+            and   type = 'U')
+   drop table PRODUCTS
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PRODUCTS2')
+            and   name  = 'PRODUCTS2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index PRODUCTS2.PRODUCTS2_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PRODUCTS2')
+            and   name  = 'PRODUCTS_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index PRODUCTS2.PRODUCTS_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('PRODUCTS2')
+            and   type = 'U')
+   drop table PRODUCTS2
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('REPORT_FORM')
+            and   type = 'U')
+   drop table REPORT_FORM
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('SEND')
+            and   name  = 'SEND2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index SEND.SEND2_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('SEND')
+            and   name  = 'SEND_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index SEND.SEND_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('SEND')
+            and   type = 'U')
+   drop table SEND
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('SHELF')
+            and   type = 'U')
+   drop table SHELF
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('STORAGE')
+            and   name  = 'STORAGE2_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index STORAGE.STORAGE2_FK
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('STORAGE')
+            and   name  = 'STORAGE_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index STORAGE.STORAGE_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('STORAGE')
+            and   type = 'U')
+   drop table STORAGE
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('SUPPLIER')
+            and   type = 'U')
+   drop table SUPPLIER
+go
+
+/*==============================================================*/
+/* Table: INVENTORY_LEVEL                                       */
+/*==============================================================*/
+create table INVENTORY_LEVEL (
+   PRODUCT_ID           int                  not null,
+   LEVE_ID              int                  not null,
+   NAME                 text                 null,
+   AMOUNT               numeric(5)           null,
+   UPDATE_AT            datetime             null,
+   EXPIRATION_DATE      datetime             null,
+   constraint PK_INVENTORY_LEVEL primary key nonclustered (PRODUCT_ID, LEVE_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: INVOICE_PAYMENT                                       */
+/*==============================================================*/
+create table INVOICE_PAYMENT (
+   PAYMENT_ID           int                  not null,
+   SUPLLIER_ID          int                  not null,
+   PAYMENTID            int                  not null,
+   SUPPLIERID           int                  not null,
+   NAME_PRODUCT         text                 null,
+   AMOUNT               numeric(5)           null,
+   PRICE                decimal(18)          null,
+   DATE                 datetime             null,
+   TOTAL                decimal(18)          null,
+   constraint PK_INVOICE_PAYMENT primary key nonclustered (PAYMENT_ID)
+)
+go
+
+/*==============================================================*/
+/* Index: DETAILS_FK                                            */
+/*==============================================================*/
+create index DETAILS_FK on INVOICE_PAYMENT (
+PAYMENTID ASC,
+SUPPLIERID ASC
+)
+go
+
+/*==============================================================*/
+/* Index: BILLS_FK                                              */
+/*==============================================================*/
+create index BILLS_FK on INVOICE_PAYMENT (
+SUPLLIER_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Table: ITEMS                                                 */
+/*==============================================================*/
+create table ITEMS (
+   SUPLLIER_ID          int                  not null,
+   PRODUCTS_ID          int                  not null,
+   constraint PK_ITEMS primary key (SUPLLIER_ID, PRODUCTS_ID)
+)
+go
+
+/*==============================================================*/
+/* Index: ITEMS_FK                                              */
+/*==============================================================*/
+create index ITEMS_FK on ITEMS (
+SUPLLIER_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Index: ITEMS2_FK                                             */
+/*==============================================================*/
+create index ITEMS2_FK on ITEMS (
+PRODUCTS_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Table: ORDERS_DETAIL                                         */
+/*==============================================================*/
+create table ORDERS_DETAIL (
+   PAYMENTID            int                  not null,
+   SUPPLIERID           int                  not null,
+   NAME_PRODUCT         text                 null,
+   AMOUNT               numeric(5)           null,
+   DATE                 datetime             null,
+   constraint PK_ORDERS_DETAIL primary key nonclustered (PAYMENTID, SUPPLIERID)
+)
+go
+
+/*==============================================================*/
+/* Table: PRODUCTS                                              */
+/*==============================================================*/
+create table PRODUCTS (
+   PRODUCTS_ID          int                  not null,
+   REPORT_ID            int                  not null,
+   NAME                 text                 null,
+   TYPE_PRODUCT         text                 null,
+   AMOUNT               numeric(5)           null,
+   SHELF_NUMBER         numeric(2)           null,
+   UPDATE_AT            datetime             null,
+   EXPIRATION_DATE      datetime             null,
+   PRICE                decimal(18)          null,
+   constraint PK_PRODUCTS primary key nonclustered (PRODUCTS_ID)
+)
+go
+
+/*==============================================================*/
+/* Index: REPORT_FK                                             */
+/*==============================================================*/
+create index REPORT_FK on PRODUCTS (
+REPORT_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Table: PRODUCTS2                                             */
+/*==============================================================*/
+create table PRODUCTS2 (
+   PRODUCTS_ID          int                  not null,
+   SHELF_ID             int                  not null,
+   constraint PK_PRODUCTS2 primary key (PRODUCTS_ID, SHELF_ID)
+)
+go
+
+/*==============================================================*/
+/* Index: PRODUCTS_FK                                           */
+/*==============================================================*/
+create index PRODUCTS_FK on PRODUCTS2 (
+PRODUCTS_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Index: PRODUCTS2_FK                                          */
+/*==============================================================*/
+create index PRODUCTS2_FK on PRODUCTS2 (
+SHELF_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Table: REPORT_FORM                                           */
+/*==============================================================*/
+create table REPORT_FORM (
+   REPORT_ID            int                  not null,
+   ID_PRODUCT           int                  null,
+   INVENTORY_LEVEL      int                  null,
+   AMOUNT               numeric(5)           null,
+   DATE                 datetime             null,
+   constraint PK_REPORT_FORM primary key nonclustered (REPORT_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: SEND                                                  */
+/*==============================================================*/
+create table SEND (
+   PAYMENTID            int                  not null,
+   SUPPLIERID           int                  not null,
+   SUPLLIER_ID          int                  not null,
+   constraint PK_SEND primary key (PAYMENTID, SUPPLIERID, SUPLLIER_ID)
+)
+go
+
+/*==============================================================*/
+/* Index: SEND_FK                                               */
+/*==============================================================*/
+create index SEND_FK on SEND (
+PAYMENTID ASC,
+SUPPLIERID ASC
+)
+go
+
+/*==============================================================*/
+/* Index: SEND2_FK                                              */
+/*==============================================================*/
+create index SEND2_FK on SEND (
+SUPLLIER_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Table: SHELF                                                 */
+/*==============================================================*/
+create table SHELF (
+   SHELF_ID             int                  not null,
+   NAME_SHELF           text                 null,
+   TYPE_PRODUCT         text                 null,
+   constraint PK_SHELF primary key nonclustered (SHELF_ID)
+)
+go
+
+/*==============================================================*/
+/* Table: STORAGE                                               */
+/*==============================================================*/
+create table STORAGE (
+   PRODUCTS_ID          int                  not null,
+   PRODUCT_ID           int                  not null,
+   LEVE_ID              int                  not null,
+   constraint PK_STORAGE primary key (PRODUCTS_ID, PRODUCT_ID, LEVE_ID)
+)
+go
+
+/*==============================================================*/
+/* Index: STORAGE_FK                                            */
+/*==============================================================*/
+create index STORAGE_FK on STORAGE (
+PRODUCTS_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Index: STORAGE2_FK                                           */
+/*==============================================================*/
+create index STORAGE2_FK on STORAGE (
+PRODUCT_ID ASC,
+LEVE_ID ASC
+)
+go
+
+/*==============================================================*/
+/* Table: SUPPLIER                                              */
+/*==============================================================*/
+create table SUPPLIER (
+   SUPLLIER_ID          int                  not null,
+   NAME                 text                 null,
+   TYPE_PRODUCT         text                 null,
+   HOTLINE              numeric(10)          null,
+   ADDRESS              text                 null,
+   constraint PK_SUPPLIER primary key nonclustered (SUPLLIER_ID)
+)
+go
+
+alter table INVOICE_PAYMENT
+   add constraint FK_INVOICE__BILLS_SUPPLIER foreign key (SUPLLIER_ID)
+      references SUPPLIER (SUPLLIER_ID)
+go
+
+alter table INVOICE_PAYMENT
+   add constraint FK_INVOICE__DETAILS_ORDERS_D foreign key (PAYMENTID, SUPPLIERID)
+      references ORDERS_DETAIL (PAYMENTID, SUPPLIERID)
+go
+
+alter table ITEMS
+   add constraint FK_ITEMS_ITEMS_SUPPLIER foreign key (SUPLLIER_ID)
+      references SUPPLIER (SUPLLIER_ID)
+go
+
+alter table ITEMS
+   add constraint FK_ITEMS_ITEMS2_PRODUCTS foreign key (PRODUCTS_ID)
+      references PRODUCTS (PRODUCTS_ID)
+go
+
+alter table PRODUCTS
+   add constraint FK_PRODUCTS_REPORT_REPORT_F foreign key (REPORT_ID)
+      references REPORT_FORM (REPORT_ID)
+go
+
+alter table PRODUCTS2
+   add constraint FK_PRODUCTS_PRODUCTS_PRODUCTS foreign key (PRODUCTS_ID)
+      references PRODUCTS (PRODUCTS_ID)
+go
+
+alter table PRODUCTS2
+   add constraint FK_PRODUCTS_PRODUCTS2_SHELF foreign key (SHELF_ID)
+      references SHELF (SHELF_ID)
+go
+
+alter table SEND
+   add constraint FK_SEND_SEND_ORDERS_D foreign key (PAYMENTID, SUPPLIERID)
+      references ORDERS_DETAIL (PAYMENTID, SUPPLIERID)
+go
+
+alter table SEND
+   add constraint FK_SEND_SEND2_SUPPLIER foreign key (SUPLLIER_ID)
+      references SUPPLIER (SUPLLIER_ID)
+go
+
+alter table STORAGE
+   add constraint FK_STORAGE_STORAGE_PRODUCTS foreign key (PRODUCTS_ID)
+      references PRODUCTS (PRODUCTS_ID)
+go
+
+alter table STORAGE
+   add constraint FK_STORAGE_STORAGE2_INVENTOR foreign key (PRODUCT_ID, LEVE_ID)
+      references INVENTORY_LEVEL (PRODUCT_ID, LEVE_ID)
+go
+
