031909bd15a9c033c38adcbd81ba37c3ce2ec3a9
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2023-10-18 23:56:45 +0700

    MainLayout

---
+++ QuanLyKhoHang/Controllers/Manage_ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyKhoHang.Controllers
+{
+    public class Manage_ProductController : Controller
+    {
+        // GET: Manage_Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
