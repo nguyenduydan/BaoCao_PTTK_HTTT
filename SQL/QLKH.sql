d58f58fcbef02bac145ddb36e7c853f2e2a2e650
FOG <dn135897@gmail.com>
2023-11-06 20:20:10 +0700

    update

--- SQL/QLKH.sql
+++ SQL/QLKH.sql
@@ -1,36 +1,8 @@
 /*==============================================================*/
 /* DBMS name:      Microsoft SQL Server 2008                    */
-/* Created on:     24/10/2023 5:14:21 CH                        */
+/* Created on:     06/11/2023 8:01:54 CH                        */
 /*==============================================================*/
 
-
-if exists (select 1
-   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('dbo.BANBAOCAO') and o.name = 'FK_BANBAOCA_BAOCAO_SANPHAM')
-alter table dbo.BANBAOCAO
-   drop constraint FK_BANBAOCA_BAOCAO_SANPHAM
-go
-
-if exists (select 1
-   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('dbo.DATVAO') and o.name = 'FK_DATVAO_DATVAO2_KEHANG')
-alter table dbo.DATVAO
-   drop constraint FK_DATVAO_DATVAO2_KEHANG
-go
-
-if exists (select 1
-   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('dbo.DATVAO') and o.name = 'FK_DATVAO_DATVAO_SANPHAM')
-alter table dbo.DATVAO
-   drop constraint FK_DATVAO_DATVAO_SANPHAM
-go
-
-if exists (select 1
-   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('dbo.DONDATHANG') and o.name = 'FK_DONDATHA_DATHANG_SANPHAM')
-alter table dbo.DONDATHANG
-   drop constraint FK_DONDATHA_DATHANG_SANPHAM
-go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
@@ -41,23 +13,58 @@ go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('dbo.HANGTON') and o.name = 'FK_HANGTON_LUUTRU_SANPHAM')
-alter table dbo.HANGTON
-   drop constraint FK_HANGTON_LUUTRU_SANPHAM
+   where r.fkeyid = object_id('dbo.DONTHANHTOAN') and o.name = 'FK_DONTHANH_DUARA_NHACUNGC')
+alter table dbo.DONTHANHTOAN
+   drop constraint FK_DONTHANH_DUARA_NHACUNGC
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('dbo.KIEMTRA') and o.name = 'FK_KIEMTRA_KIEMTRA2_DONDATHA')
-alter table dbo.KIEMTRA
-   drop constraint FK_KIEMTRA_KIEMTRA2_DONDATHA
+   where r.fkeyid = object_id('dbo.DONTHANHTOAN') and o.name = 'FK_DONTHANH_KIEMTRA_DONDATHA')
+alter table dbo.DONTHANHTOAN
+   drop constraint FK_DONTHANH_KIEMTRA_DONDATHA
 go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
-   where r.fkeyid = object_id('dbo.KIEMTRA') and o.name = 'FK_KIEMTRA_KIEMTRA_DONTHANH')
-alter table dbo.KIEMTRA
-   drop constraint FK_KIEMTRA_KIEMTRA_DONTHANH
+   where r.fkeyid = object_id('dbo.SANPHAM') and o.name = 'FK_SANPHAM_CUNGCAP_NHACUNGC')
+alter table dbo.SANPHAM
+   drop constraint FK_SANPHAM_CUNGCAP_NHACUNGC
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('dbo.SANPHAM') and o.name = 'FK_SANPHAM_LUUTRU_KEHANG')
+alter table dbo.SANPHAM
+   drop constraint FK_SANPHAM_LUUTRU_KEHANG
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('dbo.THONGTINBAOCAO') and o.name = 'FK_THONGTIN_THONGTINB_BANBAOCA')
+alter table dbo.THONGTINBAOCAO
+   drop constraint FK_THONGTIN_THONGTINB_BANBAOCA
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('dbo.THONGTINBAOCAO') and o.name = 'FK_THONGTIN_THONGTINB_SANPHAM')
+alter table dbo.THONGTINBAOCAO
+   drop constraint FK_THONGTIN_THONGTINB_SANPHAM
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('dbo.THONGTINDDH') and o.name = 'FK_THONGTIN_THONGTIND_DONDATHA')
+alter table dbo.THONGTINDDH
+   drop constraint FK_THONGTIN_THONGTIND_DONDATHA
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('dbo.THONGTINDDH') and o.name = 'FK_THONGTIN_THONGTIND_SANPHAM')
+alter table dbo.THONGTINDDH
+   drop constraint FK_THONGTIN_THONGTIND_SANPHAM
 go
 
 alter table dbo.BANBAOCAO
@@ -71,17 +78,6 @@ if exists (select 1
    drop table dbo.BANBAOCAO
 go
 
-alter table dbo.DATVAO
-   drop constraint PK_DATVAO
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('dbo.DATVAO')
-            and   type = 'U')
-   drop table dbo.DATVAO
-go
-
 alter table dbo.DONDATHANG
    drop constraint PK_DONDATHANG
 go
@@ -93,26 +89,11 @@ if exists (select 1
    drop table dbo.DONDATHANG
 go
 
-alter table dbo.DONTHANHTOAN
-   drop constraint PK_DONTHANHTOAN
-go
-
 if exists (select 1
             from  sysobjects
            where  id = object_id('dbo.DONTHANHTOAN')
             and   type = 'U')
    drop table dbo.DONTHANHTOAN
-go
-
-alter table dbo.HANGTON
-   drop constraint PK_HANGTON
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('dbo.HANGTON')
-            and   type = 'U')
-   drop table dbo.HANGTON
 go
 
 alter table dbo.KEHANG
@@ -124,17 +105,6 @@ if exists (select 1
            where  id = object_id('dbo.KEHANG')
             and   type = 'U')
    drop table dbo.KEHANG
-go
-
-alter table dbo.KIEMTRA
-   drop constraint PK_KIEMTRA
-go
-
-if exists (select 1
-            from  sysobjects
-           where  id = object_id('dbo.KIEMTRA')
-            and   type = 'U')
-   drop table dbo.KIEMTRA
 go
 
 alter table dbo.NHACUNGCAP
@@ -159,11 +129,26 @@ if exists (select 1
    drop table dbo.SANPHAM
 go
 
+alter table dbo.THONGTINBAOCAO
+   drop constraint PK_THONGTINBAOCAO
+go
+
 if exists (select 1
             from  sysobjects
-           where  id = object_id('dbo.sysdiagrams')
+           where  id = object_id('dbo.THONGTINBAOCAO')
             and   type = 'U')
-   drop table dbo.sysdiagrams
+   drop table dbo.THONGTINBAOCAO
+go
+
+alter table dbo.THONGTINDDH
+   drop constraint PK_THONGTINDDH
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('dbo.THONGTINDDH')
+            and   type = 'U')
+   drop table dbo.THONGTINDDH
 go
 
 drop schema dbo
@@ -174,45 +159,8 @@ go
 /*==============================================================*/
 create table BANBAOCAO (
    MA_BAOCAO            varchar(10)          not null,
-   MASP                 varchar(10)          not null,
-   SOLUONG              numeric(5)           null,
    NGAYBAOCAO           datetime             null,
    constraint PK_BANBAOCAO primary key nonclustered (MA_BAOCAO)
-)
-go
-
-/*==============================================================*/
-/* Index: ASSOCIATION_10_FK                                     */
-/*==============================================================*/
-create index ASSOCIATION_10_FK on BANBAOCAO (
-MASP ASC
-)
-go
-
-/*==============================================================*/
-/* Table: DATVAO                                                */
-/*==============================================================*/
-create table DATVAO (
-   MASP                 varchar(10)          not null,
-   MA_KEHANG            varchar(10)          not null,
-   SOLUONG              numeric              null,
-   constraint PK_DATVAO primary key nonclustered (MASP, MA_KEHANG)
-)
-go
-
-/*==============================================================*/
-/* Index: DATVAO2_FK                                            */
-/*==============================================================*/
-create index DATVAO2_FK on DATVAO (
-MASP ASC
-)
-go
-
-/*==============================================================*/
-/* Index: DATVAO_FK                                             */
-/*==============================================================*/
-create index DATVAO_FK on DATVAO (
-MA_KEHANG ASC
 )
 go
 
@@ -221,20 +169,10 @@ go
 /*==============================================================*/
 create table DONDATHANG (
    MA_DATHANG           varchar(10)          not null,
-   MASP                 varchar(10)          not null,
-   TEN_SP               nvarchar(100)        null,
    MA_NCCAP             varchar(10)          not null,
-   SOLUONG              numeric(5)           null,
+   LOAISP               nvarchar(100)        null,
    NGAY_DATHANG         datetime             null,
    constraint PK_DONDATHANG primary key nonclustered (MA_DATHANG)
-)
-go
-
-/*==============================================================*/
-/* Index: ASSOCIATION_7_FK                                      */
-/*==============================================================*/
-create index ASSOCIATION_7_FK on DONDATHANG (
-MASP ASC
 )
 go
 
@@ -247,64 +185,13 @@ MA_NCCAP ASC
 go
 
 /*==============================================================*/
-/* Table: DONTHANHTOAN                                          */
-/*==============================================================*/
-create table DONTHANHTOAN (
-   MATT                 varchar(10)          not null,
-   TENSP                nvarchar(100)        null,
-   NGAYTHANHTOAN        datetime             null,
-   SOLUONG              numeric(5)           null,
-   GIACA                decimal(18)          null,
-   constraint PK_DONTHANHTOAN primary key nonclustered (MATT)
-)
-go
-
-/*==============================================================*/
-/* Table: HANGTON                                               */
-/*==============================================================*/
-create table HANGTON (
-   MA_HANGTON           varchar(10)          not null,
-   MASP                 varchar(10)          not null,
-   SOLUONG              numeric(5)           null,
-   MOTA_HANGTON         nvarchar(1000)       null,
-   constraint PK_HANGTON primary key nonclustered (MA_HANGTON, MASP)
-)
-go
-
-/*==============================================================*/
 /* Table: KEHANG                                                */
 /*==============================================================*/
 create table KEHANG (
    MA_KEHANG            varchar(10)          not null,
-   TEN_KEHANG           nvarchar(100)        null,
-   MOTA_KEHANG          nvarchar(1000)       null,
+   LOAISP               nvarchar(100)        null,
+   VITRI                nvarchar(20)         null,
    constraint PK_KEHANG primary key nonclustered (MA_KEHANG)
-)
-go
-
-/*==============================================================*/
-/* Table: KIEMTRA                                               */
-/*==============================================================*/
-create table KIEMTRA (
-   MATT                 varchar(10)          not null,
-   MA_DATHANG           varchar(10)          not null,
-   constraint PK_KIEMTRA primary key nonclustered (MATT, MA_DATHANG)
-)
-go
-
-/*==============================================================*/
-/* Index: KIEMTRA2_FK                                           */
-/*==============================================================*/
-create index KIEMTRA2_FK on KIEMTRA (
-MATT ASC
-)
-go
-
-/*==============================================================*/
-/* Index: KIEMTRA_FK                                            */
-/*==============================================================*/
-create index KIEMTRA_FK on KIEMTRA (
-MA_DATHANG ASC
 )
 go
 
@@ -314,11 +201,12 @@ go
 create table NHACUNGCAP (
    MA_NCCAP             varchar(10)          not null,
    TEN_NCCAP            nvarchar(100)        null,
-   LOASP                nvarchar(100)        null,
+   LOAISP               nvarchar(100)        null,
    SDT                  numeric(10)          null,
    DIACHI               nchar(200)           null,
    EMAIL                nchar(100)           null,
-   GIACA                decimal(18)          null,
+   NGAYTAO              datetime             null,
+   NGAYCAPNHAT          datetime             null,
    constraint PK_NHACUNGCAP primary key nonclustered (MA_NCCAP)
 )
 go
@@ -328,36 +216,90 @@ go
 /*==============================================================*/
 create table SANPHAM (
    MASP                 varchar(10)          not null,
+   MA_KEHANG            varchar(10)          not null,
+   MA_NCCAP             varchar(10)          not null,
    TENSP                nvarchar(100)        null,
    LOAISP               nvarchar(100)        null,
    TENTOMTAT            varchar(100)         null,
-   SOLUONG              numeric(5)           null,
+   SOLUONG              int                  null,
    NGAYTAO              datetime             null,
    NGAYCAPNHAT          datetime             null,
    NGAYHETHAN           datetime             null,
-   GIACA                decimal(18)          null,
+   GIATIEN              decimal(18)          null,
+   TRANGTHAI            tinyint              null,
    constraint PK_SANPHAM primary key nonclustered (MASP)
 )
 go
 
-alter table BANBAOCAO
-   add constraint FK_BANBAOCA_BAOCAO_SANPHAM foreign key (MASP)
-      references SANPHAM (MASP)
+/*==============================================================*/
+/* Index: CUNGCAP_FK                                            */
+/*==============================================================*/
+create index CUNGCAP_FK on SANPHAM (
+MA_NCCAP ASC
+)
 go
 
-alter table DATVAO
-   add constraint FK_DATVAO_DATVAO_KEHANG foreign key (MA_KEHANG)
-      references KEHANG (MA_KEHANG)
+/*==============================================================*/
+/* Index: LUUTRU_FK                                             */
+/*==============================================================*/
+create index LUUTRU_FK on SANPHAM (
+MA_KEHANG ASC
+)
 go
 
-alter table DATVAO
-   add constraint FK_DATVAO_DATVAO2_SANPHAM foreign key (MASP)
-      references SANPHAM (MASP)
+/*==============================================================*/
+/* Table: THONGTINBAOCAO                                        */
+/*==============================================================*/
+create table THONGTINBAOCAO (
+   MASP                 varchar(10)          not null,
+   MA_BAOCAO            varchar(10)          not null,
+   TENSP                nvarchar(100)        null,
+   SOLUONG              int                  null,
+   constraint PK_THONGTINBAOCAO primary key nonclustered (MASP, MA_BAOCAO)
+)
 go
 
-alter table DONDATHANG
-   add constraint FK_DONDATHA_DATHANG_SANPHAM foreign key (MASP)
-      references SANPHAM (MASP)
+/*==============================================================*/
+/* Index: THONGTINBAOCAO2_FK                                    */
+/*==============================================================*/
+create index THONGTINBAOCAO2_FK on THONGTINBAOCAO (
+MASP ASC
+)
+go
+
+/*==============================================================*/
+/* Index: THONGTINBAOCAO_FK                                     */
+/*==============================================================*/
+create index THONGTINBAOCAO_FK on THONGTINBAOCAO (
+MA_BAOCAO ASC
+)
+go
+
+/*==============================================================*/
+/* Table: THONGTINDDH                                           */
+/*==============================================================*/
+create table THONGTINDDH (
+   MA_DATHANG           varchar(10)          not null,
+   MASP                 varchar(10)          not null,
+   SOLUONG              int                  null,
+   constraint PK_THONGTINDDH primary key nonclustered (MA_DATHANG, MASP)
+)
+go
+
+/*==============================================================*/
+/* Index: THONGTINDDH2_FK                                       */
+/*==============================================================*/
+create index THONGTINDDH2_FK on THONGTINDDH (
+MA_DATHANG ASC
+)
+go
+
+/*==============================================================*/
+/* Index: THONGTINDDH_FK                                        */
+/*==============================================================*/
+create index THONGTINDDH_FK on THONGTINDDH (
+MASP ASC
+)
 go
 
 alter table DONDATHANG
@@ -365,18 +307,43 @@ alter table DONDATHANG
       references NHACUNGCAP (MA_NCCAP)
 go
 
-alter table HANGTON
-   add constraint FK_HANGTON_LUUTRU_SANPHAM foreign key (MASP)
+alter table SANPHAM
+   add constraint FK_SANPHAM_CUNGCAP_NHACUNGC foreign key (MA_NCCAP)
+      references NHACUNGCAP (MA_NCCAP)
+go
+
+alter table SANPHAM
+   add constraint FK_SANPHAM_LUUTRU_KEHANG foreign key (MA_KEHANG)
+      references KEHANG (MA_KEHANG)
+go
+
+alter table THONGTINBAOCAO
+   add constraint FK_THONGTIN_THONGTINB_BANBAOCA foreign key (MA_BAOCAO)
+      references BANBAOCAO (MA_BAOCAO)
+go
+
+alter table THONGTINBAOCAO
+   add constraint FK_THONGTIN_THONGTINB_SANPHAM foreign key (MASP)
       references SANPHAM (MASP)
 go
 
-alter table KIEMTRA
-   add constraint FK_KIEMTRA_KIEMTRA_DONDATHA foreign key (MA_DATHANG)
+alter table THONGTINDDH
+   add constraint FK_THONGTIN_THONGTIND_SANPHAM foreign key (MASP)
+      references SANPHAM (MASP)
+go
+
+alter table THONGTINDDH
+   add constraint FK_THONGTIN_THONGTIND_DONDATHA foreign key (MA_DATHANG)
       references DONDATHANG (MA_DATHANG)
 go
 
-alter table KIEMTRA
-   add constraint FK_KIEMTRA_KIEMTRA2_DONTHANH foreign key (MATT)
-      references DONTHANHTOAN (MATT)
+
+create trigger CLR_TRIGGER_DONDATHANG on DONDATHANG  insert as
+external name %Assembly.GeneratedName%.
+go
+
+
+create trigger CLR_TRIGGER_SANPHAM on SANPHAM  insert as
+external name %Assembly.GeneratedName%.
 go
 
